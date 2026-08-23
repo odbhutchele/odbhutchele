a113be276aaf765117963c5bd6dffda80eec4a0d
Adi Islam <62735486+odbhutchele@users.noreply.github.com>
2026-08-23 17:53:02 +0600

    Update today.py

--- today.py
+++ today.py
@@ -289,13 +289,11 @@ def force_close_file(data, cache_comment):
 
 
 def stars_counter(data):
-    """
-    Count total stars in repositories owned by me
-    """
     total_stars = 0
-    for node in data: total_stars += node['node']['stargazers']['totalCount']
+    for node in data:
+        if node['node'] is not None:
+            total_stars += node['node']['stargazers']['totalCount']
     return total_stars
-
 
 def svg_overwrite(filename, age_data, commit_data, star_data, repo_data, contrib_data, follower_data, loc_data):
     """
